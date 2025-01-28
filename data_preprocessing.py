db638dfbfd4c29db485feb1daf2e2d93c7f7b8c6
eleyterokokus <eleyterokokus@gmail.com>
2025-01-28 19:10:26 +1100

    data preprocessing

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,54 +1,56 @@
 import pandas as pd
+from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
-from sklearn.impute import SimpleImputer
 
-def load_and_preprocess(file_path: str = "data/cars_raw.csv"):
-
+def create_and_save_processed_dataframe(input_file_path: str = "./data/cars_raw.csv", output_file_path: str = "./data/prep_data.csv"):
     # Загрузка данных
-    df = pd.read_csv(file_path)
+    df = pd.read_csv(input_file_path)
 
-    # Очистка данных: удаление пропущенных значений
+    # Проверяем наличие колонки 'Price'
+    if 'Price' not in df.columns:
+        raise ValueError("Столбец 'Price' не найден в данных.")
+
+    # Удаление строк с пропущенными значениями
     df.dropna(inplace=True)
 
     # Разделяем данные на признаки и целевую переменную
     X = df.drop('Price', axis=1)
     y = df['Price']
 
-    # Инициализируем списки для числовых и категориальных признаков
+    # Определяем числовые и категориальные признаки
     numeric_features = []
     categorical_features = []
 
-    # Проходим по всем столбцам DataFrame и определяем их тип
-    for column in df.columns:
-        if pd.api.types.is_numeric_dtype(df[column]):
+    for column in X.columns:
+        if pd.api.types.is_numeric_dtype(X[column]):
             numeric_features.append(column)
-        elif pd.api.types.is_categorical_dtype(df[column]) or pd.api.types.is_object_dtype(df[column]):
+        elif pd.api.types.is_object_dtype(X[column]) or isinstance(X[column].dtype, pd.CategoricalDtype):
             categorical_features.append(column)
 
-            # Создаем пайплайн для предобработки данных
+    # Создаем пайплайн для предобработки данных
     numeric_transformer = Pipeline(steps=[
         ('imputer', SimpleImputer(strategy='mean')),  # Заполнение пропусков средним значением
         ('scaler', StandardScaler())                   # Стандартизация числовых признаков
     ])
 
-    categorical_transformer = Pipeline(steps=[
-        ('imputer', SimpleImputer(strategy='most_frequent')),  # Заполнение пропусков наиболее частым значением
-        ('onehot', OneHotEncoder(handle_unknown='ignore'))      # Кодирование категориальных признаков
-    ])
-
-    # Объединяем трансформеры в один ColumnTransformer
+    # # Объединяем трансформеры в один ColumnTransformer
     preprocessor = ColumnTransformer(
         transformers=[
-            ('num', numeric_transformer, numeric_features),
-            ('cat', categorical_transformer, categorical_features)
+            ('num', numeric_transformer, numeric_features)
         ]
     )
+
     # Применяем предобработку к данным
-    new_df = preprocessor.fit_transform(df)
+    features_normalized = preprocessor.fit_transform(X)
 
-    # Сохранение данных
-    new_df.to_csv("./data/prep_data.csv", index=False)
+    # Преобразование нормализованных признаков в DataFrame
+    processed_data = pd.DataFrame(
+        features_normalized, columns=numeric_features)
 
-load_and_preprocess()
+    # Добавление целевого признака 'Price'
+    processed_data['Price'] = y.reset_index(drop=True)
+
+    # Сохранение обработанных данных в файл
+    processed_data.to_csv(output_file_path, index=False)
